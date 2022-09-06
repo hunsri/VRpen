6bb5bba650ce2b054abecdc1afacb32e3e1972a4
hunsri <richard.hunstock@gmx.de>
2022-09-06 17:33:52 +0200

    added function to spawn in objects

---
+++ Assets/VRpen/Scripts/ButtonInput.cs
@@ -0,0 +1,44 @@
+﻿/* Copyright (c) Logitech Corporation. All rights reserved. Licensed under the MIT License.*/
+
+ namespace VRpen.Scripts
+{
+    using Logitech.Scripts;
+    using UnityEngine;
+    using Valve.VR;
+    using System;
+
+    /// <summary>
+    /// Update the colour of a Stylus button when it is pressed.
+    /// </summary>
+    public class ButtonInput : MonoBehaviour
+    {
+        [Header("Input")]
+        public bool GetInputSourceFromStylusDetection = true;
+        [Tooltip("If not using UseStylusDetection, set the SteamVR input source manually")]
+        public SteamVR_Input_Sources ManualSteamVRInputSource;
+        [SerializeField]
+        private SteamVR_Action_Boolean _input;
+
+
+        public event Action OnSpawnObject = delegate { };
+
+        private void Update()
+        {
+            SteamVR_Input_Sources inputSource = GetInputSourceFromStylusDetection
+                ? PrimaryDeviceDetection.PrimaryDeviceBehaviourPose.inputSource
+                : ManualSteamVRInputSource;
+
+            if (_input.GetStateDown(inputSource))
+            {
+                //Debug.Log("button down");
+
+                OnSpawnObject();
+            }
+
+            if (_input.GetStateUp(inputSource))
+            {
+                //Debug.Log("button up");
+            }
+        }
+    }
+}
---
+++ Assets/VRpen/Scripts/SpawnObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+using VRSketchingGeometry.SketchObjectManagement;
+using VRSketchingGeometry;
+using VRSketchingGeometry.Commands;
+using VRSketchingGeometry.Commands.Line;
+
+namespace VRpen.Scripts
+{
+    public class SpawnObject : MonoBehaviour
+    {
+        public DefaultReferences Defaults;
+        private LineSketchObject LineSketchObject;
+        private SketchWorld SketchWorld;
+        private CommandInvoker Invoker;
+
+        private void Awake()
+        {
+            GetComponent<ButtonInput>().OnSpawnObject += HandleSpawnObject;
+        }
+
+        private void HandleSpawnObject()
+        {
+            Debug.Log("Spawning!");
+            SketchWorld = Instantiate(Defaults.SketchWorldPrefab).GetComponent<SketchWorld>();
+            LineSketchObject = Instantiate(Defaults.LineSketchObjectPrefab).GetComponent<LineSketchObject>();
+            Invoker = new CommandInvoker();
+            Invoker.ExecuteCommand(new AddObjectToSketchWorldRootCommand(LineSketchObject, SketchWorld));
+            Invoker.ExecuteCommand(new AddControlPointCommand(this.LineSketchObject, new Vector3(1, 1, 1)));
+            Invoker.ExecuteCommand(new AddControlPointCommand(this.LineSketchObject, new Vector3(0, 1, 0)));
+            Invoker.ExecuteCommand(new AddControlPointCommand(this.LineSketchObject, new Vector3(0, 1, -1)));
+            Invoker.ExecuteCommand(new AddControlPointCommand(this.LineSketchObject, new Vector3(1, 1, 1)));
+            Invoker.Undo();
+            
+        }
+    }
+}
